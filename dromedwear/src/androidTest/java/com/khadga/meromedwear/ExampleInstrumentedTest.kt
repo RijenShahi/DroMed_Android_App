d23c17651d91c91557efab28554d03ae1b5a8ad2
RijenShahi <50471796+RijenShahi@users.noreply.github.com>
2021-10-04 17:58:36 +0545

    EHS bassed DroMed Android Application

--- dromedwear/src/androidTest/java/com/khadga/meromedwear/ExampleInstrumentedTest.kt
+++ dromedwear/src/androidTest/java/com/khadga/meromedwear/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.khadga.meromedwear", appContext.packageName)
+        assertEquals("com.khadga.dromedwear", appContext.packageName)
     }
 }
